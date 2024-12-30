cc95ee599e6d72ab525aefa8dbfed0d9797701f2
Rashika1810 <rashika.kr04@gmail.com>
2024-12-30 12:16:04 +0530

    minor changes

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -6,17 +6,11 @@ export interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      {/* Header */}
-      {/* <Header /> */}
-
       {/* Main Content Area */}
       <div className="p-4">
         {/* Main Page Content */}
         <main className="">{children}</main>
       </div>
-
-      {/* Footer */}
-      {/* <Footer /> */}
     </div>
   );
 };
